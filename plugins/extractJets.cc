bddee29e956b8e89dd18d2e081ff998cfdc83ecb
Braden Kronheim <brkronheim@davidson.edu>
2020-08-26 12:45:03 -0400

    Initial Files

---
+++ plugins/extractJets.cc
@@ -0,0 +1,197 @@
+// -*- C++ -*-
+//
+// Package:    ExoAnalysis/WR_MASS_PLOT
+// Class:      WR_MASS_PLOT
+//
+/**\class WR_MASS_PLOT WR_MASS_PLOT.cc ExoAnalysis/WR_MASS_PLOT/plugins/WR_MASS_PLOT.cc
+ Description: [one line class summary]
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Andrew Evans
+//         Created:  Mon, 14 Oct 2019 19:43:16 GMT
+//
+//
+
+
+// system include files
+#include <memory>
+#include <fstream>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+ #include "FWCore/Utilities/interface/InputTag.h"
+ #include "DataFormats/TrackReco/interface/Track.h"
+ #include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/JetReco/interface/GenJet.h"
+
+#include "DataFormats/PatCandidates/interface/VIDCutFlowResult.h"
+
+#include "HEEP/VID/interface/CutNrs.h"
+#include "HEEP/VID/interface/VIDCutCodes.h"
+
+#include "TLorentzVector.h"
+#include <TMatrixDSym.h>
+#include <TMatrixDSymEigen.h>
+#include <TVectorD.h>
+#include <cmath>
+
+#include "TH1.h"
+
+#include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
+
+#include "ExoAnalysis/WR_lite/interface/eventBits.h"
+#include "ExoAnalysis/WR_lite/interface/eventInfo.h"
+#include "ExoAnalysis/WR_lite/interface/eventHistos.h"
+#include "ExoAnalysis/WR_lite/interface/neuralNet.h"
+
+//
+// class declaration
+//
+
+// If the analyzer does not use TFileService, please remove
+// the template argument to the base class so the class inherits
+// from  edm::one::EDAnalyzer<>
+// This will improve performance in multithreaded jobs.
+
+
+using reco::TrackCollection;
+
+class WR_MASS_PLOT : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
+	public:
+		explicit WR_MASS_PLOT(const edm::ParameterSet&);
+		~WR_MASS_PLOT();
+
+		static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+	private:
+		virtual void beginJob() override;
+		virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+		virtual void endJob() override;
+		double dR2(double eta1, double eta2, double phi1, double phi2);
+		double dPhi(double phi1, double phi2);
+		double transverseSphericity(math::XYZTLorentzVector p1, math::XYZTLorentzVector p2, math::XYZTLorentzVector p3);
+		void saveElectronData(eventBits * myEvent, double matched1Mass, double matched2Mass);
+		void saveMuonData(eventBits * myEvent, double matched1Mass, double matched2Mass);
+		
+		
+		eventHistos m_allEvents;
+		neuralNet networkResolved = neuralNet("/home/kronh006/Version3/CMSSW_10_4_0_patch1/src/ExoAnalysis/WR_lite/data/Resolved");
+		neuralNet networkSuperResolved = neuralNet("/home/kronh006/Version3/CMSSW_10_4_0_patch1/src/ExoAnalysis/WR_lite/data/SuperResolved");
+
+		// ----------member data ---------------------------
+
+		edm::EDGetTokenT<TrackCollection> tracksToken_;  //used to select what tracks to read from configuration file
+		edm::EDGetToken m_genParticleToken;
+		edm::EDGetToken m_highMuonToken;
+		edm::EDGetToken m_highElectronToken;
+		edm::EDGetToken m_AK4recoCHSJetsToken;
+		edm::EDGetToken m_genEventInfoToken;
+		edm::EDGetToken m_offlineVerticesToken;
+		
+		std::string m_dataSaveFile;
+		bool m_isSignal;
+		bool m_genTrainData;
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+WR_MASS_PLOT::WR_MASS_PLOT(const edm::ParameterSet& iConfig)
+	:
+	tracksToken_(consumes<TrackCollection>(iConfig.getUntrackedParameter<edm::InputTag>("tracks"))),
+	m_genParticleToken(consumes<std::vector<reco::GenParticle>> (iConfig.getParameter<edm::InputTag>("genParticles"))),
+	m_highMuonToken (consumes<std::vector<pat::Muon>> (iConfig.getParameter<edm::InputTag>("highMuons"))),
+	m_highElectronToken (consumes<std::vector<pat::Electron>> (iConfig.getParameter<edm::InputTag>("highElectrons"))),
+	m_AK4recoCHSJetsToken (consumes<std::vector<pat::Jet>> (iConfig.getParameter<edm::InputTag>("AK4recoCHSJets"))),
+	m_genEventInfoToken (consumes<GenEventInfoProduct> (iConfig.getParameter<edm::InputTag>("genInfo"))),
+	m_offlineVerticesToken (consumes<std::vector<reco::Vertex>> (iConfig.getParameter<edm::InputTag>("vertices"))),
+	m_dataSaveFile (iConfig.getUntrackedParameter<std::string>("trainFile")),
+	m_isSignal (iConfig.getUntrackedParameter<bool>("isSignal")),
+	m_genTrainData (iConfig.getUntrackedParameter<bool>("genTrainData"))
+{
+   //now do what ever initialization is needed
+}
+
+
+WR_MASS_PLOT::~WR_MASS_PLOT()
+{
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void
+WR_MASS_PLOT::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+	
+	edm::Handle<std::vector<reco::GenParticle>> genParticles;
+	iEvent.getByToken(m_genParticleToken, genParticles);
+
+	
+	std::cout << "Gen particles" << std::endl;
+	for (std::vector<reco::GenParticle>::const_iterator iParticle = genParticles->begin(); iParticle != genParticles->end(); iParticle++) {
+		std::cout << "STATUS: " << iParticle->status() << " PDGID: " << iParticle->pdgId() << " MOTHER: " << iParticle->mother()->pdgId() << std::endl;
+	}
+
+}
+// ------------ method called once each job just before starting event loop  ------------
+void
+WR_MASS_PLOT::beginJob() {
+	edm::Service<TFileService> fs; 
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void
+WR_MASS_PLOT::endJob() {
+	
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+WR_MASS_PLOT::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+
+  //Specify that only 'tracks' is allowed
+  //To use, remove the default given above and uncomment below
+  //ParameterSetDescription desc;
+  //desc.addUntracked<edm::InputTag>("tracks","ctfWithMaterialTracks");
+  //descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(extractJets);
---
+++ python/cfg.py
@@ -0,0 +1,30 @@
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing ('analysis')
+options.parseArguments()
+
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        options.inputFiles
+    )
+)
+
+process.demo = cms.EDAnalyzer('WR_MASS_PLOT',
+  genParticles = cms.InputTag("genParticles"),
+
+)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string(options.outputFile)
+)
+
+process.p = cms.Path(process.demo)
