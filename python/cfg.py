bca94c3449e33f3711ea823ac469e457aad6a159
jblue1 <john.s.blue98@gmail.com>
2020-08-28 20:33:47 +0200

    only process first event

--- python/cfg.py
+++ python/cfg.py
@@ -7,9 +7,8 @@ options.parseArguments()
 
 process = cms.Process("Demo")
 
-process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
